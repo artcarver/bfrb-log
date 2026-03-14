7c62d579be09c7c152209ca588482dc4a322bd9a
artcarver <ghstilson@gmail.com>
2026-03-13 18:24:13 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
  * ────────────────────────────────────────────────────────────────
  */
 
-const CACHE  = 'gw-shell-v15';
+const CACHE  = 'gw-shell-v16';
 const SHELL  = [
   './',
   './index.html',
